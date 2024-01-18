df6321dc7685348e6731aad191b4ac3cbe9b8abf
rohit.saroha <rohit.saroha@okoders.com>
2024-01-18 11:48:49 +0530

    ADD: First commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function About() {
+  return (
+    <p>About Page</p>
+  )
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Blog() {
+  return (
+    <p>Blog Page</p>
+  )
+}
